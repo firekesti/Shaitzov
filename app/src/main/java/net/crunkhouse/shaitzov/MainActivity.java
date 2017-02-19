590c8f6b5b023ee0d63857940a328a0887e1422c
Kate Kelly <firekesti@users.noreply.github.com>
2017-02-19 15:20:13 -0500

    initial commit

---
+++ app/src/main/java/net/crunkhouse/shaitzov/MainActivity.java
@@ -0,0 +1,15 @@
+package net.crunkhouse.shaitzov;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
